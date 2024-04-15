6e3757d011df4cead61623d566393639d8bf94bc
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-04-15 07:56:17 -0300

    feat: Implement delete  method

--- src/main/java/com/muccio/controllers/ParkingSpotController.java
+++ src/main/java/com/muccio/controllers/ParkingSpotController.java
@@ -58,5 +58,15 @@ public class ParkingSpotController {
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
                         .body("Parking Spot not found."));
     }
-    
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value = "id") UUID id) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+        }
+        parkingSpotService.delete(parkingSpotModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully.");
+    }
+
 }
